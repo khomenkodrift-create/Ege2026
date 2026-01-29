f26b91583b8d2237f6bc0384f6e23179b391ceb8
khomenkodrift-create <khomenko.drift@gmail.com>
2026-01-29 15:11:20 +0200

    дз

---
+++ Task-6/Task-23552.py
@@ -0,0 +1,37 @@
+from turtle import *
+
+screensize(5000, 5000)
+tracer(False)
+
+m = 10
+
+for i in range(5):
+    fd(37 * m)
+    rt(90)
+    fd(44 * m)
+    rt(90)
+
+up()
+
+bk(18 * m)
+rt(90)
+fd(29 * m)
+lt(90)
+
+down()
+
+for i in range(5):
+    fd(31 * m)
+    rt(90)
+    fd(35 * m)
+    rt(90)
+
+up()
+
+for x in range(1, 38):
+    for y in range(-15, 1):
+        goto(x * m, y * m)
+        dot(3, 'red')
+
+update()
+done()
