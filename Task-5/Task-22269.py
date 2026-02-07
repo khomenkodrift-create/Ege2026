3e4b160a0e6f7cc7a2a360926de55bc0c5fb76ef
unoxo_ <khomenk.drift@gmail.com>
2026-02-07 19:35:35 +0200

    урок

--- Task-5/Task-22269.py
+++ Task-5/Task-22269.py
@@ -10,9 +10,10 @@ def convert(num, sys):
 for N in range(1, 100_000):
     if N % 5 == 0:
         R = convert(N, 5)
-        R =  '33' + R.replace('1', '4'), R.replace('4', '1')
+        R =  '33' + R.replace('1', '*')
+        R.replace('4', '1')
     else:
-        R = R.replace([0,0], 3) + 42
+        R = R.replace(
     if R > 1922:
         ans.append(N)
 
--- Task-5/Task-23264.py
+++ Task-5/Task-23264.py
@@ -1,11 +1,11 @@
-ans = []
-
 def convert(num, sys):
     res = ''
     while num:
         res += str(num % sys)
         num //= sys
     return res[::-1]
+
+ans = []
 
 for N in range(1, 100_000):
     R = convert(N, 3)
