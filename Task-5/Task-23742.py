824f79c33f9653c0f6b488198efb984474fffb98
ivank <khomenko.drift@gmail.com>
2026-02-07 17:32:43 +0300

    дз done (very hard)

--- Task-5/Task-23742.py
+++ Task-5/Task-23742.py
@@ -3,4 +3,11 @@ ans = []
 for N in range(1, 100_000):
     R = bin(N)[2:]
     if N % 3 == 0:
-        R = R + sum(map()
+        R += R[-3:]
+    else:
+        R += bin(N % 3 * 3)[2:]
+    R = int(R, 3)
+    if R > 200:
+        ans.append(R)
+
+print(min(ans)) # 274
