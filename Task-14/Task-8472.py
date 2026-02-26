360a71d18f02981fb6efe5b541e3bda9b91881c5
khomenkodrift-create <khomenko.drift@gmail.com>
2026-02-26 09:51:43 +0200

    no comments...

---
+++ Task-14/Task-8472.py
@@ -0,0 +1,15 @@
+def convert(num,sys):
+    res = ''
+    while num:
+        res += str(num%sys)
+        num //= sys
+    return res [::-1]
+
+from string import printable
+for x in printable[1:100]:
+    num_1 = int(f'7A{x}0123', 100)
+    num_2 = int(f'1B{x}A64{x}', 100)
+    num_3 = int(f'{x}', 100)
+    num = num_1 + num_2 + num_3
+    if num % 21 ==0:
+        print(num // 21)
---
+++ Task-5/Task-20883.py
@@ -0,0 +1,24 @@
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
+
+
+ans = []
+for N in range(1, 100_000):
+    R = convert(N, 5)
+    if len(str(R)) % 2 == 0:
+        mid_1 = len(str(R)) // 2
+        R = R[:mid_1] + R[mid_1:]
+    else:
+        R = R + str(N % 5)
+        mid_2 = len(str(R)) // 2
+        R = R[:mid_2] + R[mid_2:]
+    R = int(R, 5)
+    if R > 50:
+        ans.append(N)
+print(min(ans))
+
+# в ответах 26, а тут 25
