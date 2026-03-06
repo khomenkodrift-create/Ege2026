c86bb7a15465f82cec70f85b42243afe02c3e937
khomenkodrift-create <khomenko.drift@gmail.com>
2026-03-06 18:40:52 +0200

    no comments...

--- Task-8/Task-24888.py
+++ Task-8/Task-24888.py
@@ -3,9 +3,6 @@ from itertools import product
 cnt = 0
 for val in product(printable[:16], repeat=4):
     val = ''.join(val)
-    if val[0] != '0' and val.count('3') == 1 and len(val) == len(set(val)):
-        for i in printable[:16]:
-            val = val.replace(i, '*')
-        if '**' not in val and '__' not in val:
+    if val[0] != '0' and val.count('3') == 1 and all(val[i] != val[i + 1] for i in range(len(val) - 1)):
             cnt += 1
 print(cnt)
---
+++ Task-8/my_theory.py
@@ -0,0 +1,12 @@
+# all(val[i] != val[i + 1] for i in range(len(val) - 1)) - никакие две одинаковые
+# не стоят рядом
+
+# val[-1] not in '26' - последний символ не может быть 2 или 6
+
+#    if val[0] != '0' and len(val) == len(set(val)): - все цифры различны и никакие две
+#        for i in printable[:16:2]:                    чётные или две нечётные цифры не стоят рядом
+#           val = val.replace(i, '*')
+#        for i in printable[1:16:2]:
+#            val = val.replace(i, '_')
+#        if '**' not in val and '__' not in val:
+
