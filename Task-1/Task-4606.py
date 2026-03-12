ac9646c5139ae5784b77afd428656a4bb147d3c2
khomenkodrift-create <khomenko.drift@gmail.com>
2026-03-12 11:20:23 +0200

    no comments...

---
+++ Task-1/Task-4606.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+graph = 'CE EG GF FA AC CD DH HE AB BF'.split()
+matrix = '68 47 45 237 368 15 248 157'.split()
+
+print(*range(1, 9))
+
+for i in permutations('ABCDEFGH'):
+    if all(str(i.index(x)+ 1) in matrix[i.index(y)] for x , y in graph):
+        print(*i)
+#18
