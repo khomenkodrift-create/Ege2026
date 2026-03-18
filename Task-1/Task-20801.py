4d4606bedfec24c04ce856ce240cba683f514edf
ivank <khomenko.drift@gmail.com>
2026-03-18 18:00:09 +0300

    дз

---
+++ Task-1/Task-20801.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+graph = 'AC CG GB BD DE EA EF FC GF'.split()
+matrix = '26 147 456 237 37 13 245'.split()
+print(*range(1, 8))
+for i in permutations('ABCDEFG'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
+        #60
