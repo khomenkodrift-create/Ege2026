7b55c54c1ad4b97f000b4de91a9defede47c9b5e
ivank <khomenko.drift@gmail.com>
2026-03-19 21:27:03 +0300

    дз

---
+++ DZ-probnik/Task-6.py
@@ -0,0 +1,41 @@
+from turtle import *
+
+screensize(5000, 5000)
+tracer(False)
+m = 15
+
+fd(30 * m)
+lt(60)
+fd(24 * m)
+rt(240)
+
+fd(54 * m)
+lt(120)
+fd(24 * m)
+lt(60)
+
+up()
+
+fd(30 * m)
+rt(90)
+fd(20 * m)
+lt(90)
+
+down()
+
+for i in range(17):
+    fd(6 * m)
+    lt(90)
+    fd(80 * m)
+    lt(90)
+
+up()
+
+for x in range(-100, 100):
+    for y in range(-100, 100):
+        goto(x * m, y * m)
+        dot(8, 'white')
+
+update()
+done()
+# 49 (+1 к каждой стороне)
