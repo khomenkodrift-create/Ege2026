2ab8b5891d9dfeb8bfef207db2591e3e17ef18bd
khomenkodrift-create <khomenko.drift@gmail.com>
2026-03-26 11:14:14 +0200

    no comments...

--- Task-16/Task-25400.py
+++ Task-16/Task-25400.py
@@ -8,10 +8,10 @@ def G(n): #27 (тк >= 28) ->
     if n >= 28: return G(n - 5) - 15
     return 3 * n - 4
 
-for i in range():
+for i in range(27, 31053):
     G(i)
 
-for i in range():
+for i in range(31054, 15, -1):
     F(i)
 
 print(F(15))
