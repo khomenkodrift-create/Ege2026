8d7aa285a3eab18541943b270f26568ff7929191
ivank <khomenko.drift@gmail.com>
2026-04-07 20:39:02 +0300

    дз

---
+++ Task-1/Task-27756(2).py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+graph = 'GE EA AH HB BF FG GD DE AC CH CF'.split()
+matrix = '247 148 467 123 68 358 13 256'.split()
+
+print(*range(1, 9))
+for i in permutations('ABCDEFGH'):
+    if all(str(i.index(x) + 1) in matrix[i.index(y)] for x, y in graph):
+        print(*i)
+        #65
