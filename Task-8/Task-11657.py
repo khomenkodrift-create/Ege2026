e13fe8e4569c365ed78c6ce27dcacde1d8e0a9c8
khomenkodrift-create <khomenko.drift@gmail.com>
2026-04-22 09:40:13 +0200

    no comments...

--- Task-8/Task-11657.py
+++ Task-8/Task-11657.py
@@ -4,7 +4,7 @@ cnt = 0
 for val in product(printable[:8], repeat=6):
     val = ''.join(val)
     if val[0] != 0 and '3' not in val and len(val) == len(set(val)):
-        for i in printable[:8:2]:
+        for i in '02468':
             val = val.replace(i, '*')
         if '**' in val:
             cnt += 1
--- Task-8/Task-12462.py
+++ Task-8/Task-12462.py
@@ -1,6 +1,24 @@
-from itertools import combinations
+from itertools import combinations, product
+from string import printable
 range_ = range(16)
 val_3 = len(list(combinations(range_, 3)))
 val_5 = len(list(combinations(range_, 5)))
 
 print(val_3 + val_5)
+
+#или
+
+cnt = 0
+for a in printable[:16]:
+    for b in printable[:16]:
+        for c in printable[:16]:
+            if a > b > c:
+                cnt += 1
+for a in printable[:16]:
+    for b in printable[:16]:
+        for c in printable[:16]:
+            for d in printable[:16]:
+                for e in printable[:16]:
+                    if a > b > c > d > e:
+                        cnt += 1
+print(cnt)
