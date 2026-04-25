841d4fa66599825e3a86ff971d59aef8a4f996f8
unoxo_ <khomenk.drift@gmail.com>
2026-04-25 19:52:15 +0200

    урок

--- Task-15/Task-4989.py
+++ Task-15/Task-4989.py
@@ -6,7 +6,7 @@ def f(x):
     return B <= (DEL(x, 17) <= A)
 
 for A1 in range(1, 1000):
-    for A2 in range(1, 1000):
+    for A2 in range(A1 + 1, 1000):
         if all(f(x) for x in range(1, 1000)):
             print(A2 - A1)
             break
