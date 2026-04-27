5e038540fa1edc86f5ae5229cb3a05402ae123ae
khomenkodrift-create <khomenko.drift@gmail.com>
2026-04-27 11:15:43 +0200

    no comments...

--- Task-16/Task-25355.py
+++ Task-16/Task-25355.py
@@ -1,14 +1,14 @@
 from functools import lru_cache
 
-def F(n):
-    if n >= 19: return F(n - 4) + 3580
-    return 6 * (G(n - 7)- 36)
+def f(n):
+    if n >= 19: return f(n - 4) + 3580
+    return 6 * (g(n - 7)- 36)
 @lru_cache(None)
-def G(n):
+def g(n):
     if n >= 248045: return n / 20 + 28
-    return G(n + 9) - 4
+    return g(n + 9) - 4
 
-for i in range(248045, 1, -1):
-    G(i)
+for i in range(248045, 10, -1):
+    g(i)
 
-print(F(637))
+print(f(673))
