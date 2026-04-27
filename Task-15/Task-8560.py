be76bc3419fb7ac9e0e9404eab7a40eb253f6884
khomenkodrift-create <khomenko.drift@gmail.com>
2026-04-27 11:00:32 +0200

    no comments...

--- Task-15/Task-8560.py
+++ Task-15/Task-8560.py
@@ -1,5 +1,6 @@
 def f(x, y, z):
     return (2 * x + y != 136) or (z * y < 100) or (A ** 2 >= x + y)
-
+for A in range(1, 1000):
+    if all(f(x, 136 - 2 * x, z) for x in range(0, 1000) for z in range(0, 1000)):
         print(A)
         break
