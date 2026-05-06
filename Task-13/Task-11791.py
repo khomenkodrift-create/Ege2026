f668dc50fed31731af7fb2840265c7046aaf3d81
unoxo_ <khomenk.drift@gmail.com>
2026-05-06 19:34:34 +0200

    урок

---
+++ Task-13/Task-11791.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+
+
+def f(ip):
+    ip = f'{int(ip):032b}'
+    return ip[:16].count('0') <= ip[16:].count('0')
+
+for mask in range(16, 24):
+    net = ip_network(f'246.51.128.202/{mask}', False)
+    if all(f(ip) for ip in net):
+        print(net.netmask)
+        #254
