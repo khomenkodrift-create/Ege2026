e0fa6c7ccaa8fd3f5f20c401d9cdb1006b4e5757
khomenkodrift-create <khomenko.drift@gmail.com>
2026-05-07 11:18:02 +0200

    no comments...

---
+++ Task-13/Task-11789.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+
+
+def f(ip):
+    ip = f'{int(ip):032b}'
+    return ip[:16].count('1') <= ip[16:].count('1')
+
+for mask in range(16, 24):
+    net = ip_network(f'99.8.254.232/{mask}', False)
+    if all(f(ip) for ip in net):
+        print(net.netmask)
+        #248
---
+++ Task-13/Task-12451.py
@@ -0,0 +1,10 @@
+from ipaddress import ip_network
+def f(ip):
+    ip = f'{int(ip):032b}'
+    return ip[16:24].count('0') > ip[24:32].count('0')
+cnt = 0
+for A in range(256):
+    net = ip_network(f'246.81.65.{A}/255.255.255.224', False)
+    if all(f(ip) for ip in net):
+        cnt += 1
+print(cnt) #неверно + не понял
