7b35b51f7c35938b2fdbcb0fa1638f0aafa83808
ivank <khomenko.drift@gmail.com>
2026-05-09 03:37:01 +0300

    дз

---
+++ Task-13/Task-11774.py
@@ -0,0 +1,9 @@
+from ipaddress import ip_network
+net = ip_network(f'214.96.0.0/255.240.0.0')
+
+cnt = 0
+for ip in net:
+    ip = f'{int(ip):032b}'
+    if ip.count('0') % 3 == 0:
+        cnt += 1
+print(cnt)
---
+++ Task-13/Task-11788.py
@@ -0,0 +1,10 @@
+from ipaddress import ip_network
+def f(ip):
+    ip = f'{int(ip):032b}'
+    return ip[:16].count('1') >= ip[16:].count('1')
+
+for mask in range(16, 24):
+    net = ip_network(f'187.124.21.237/{mask}', False)
+    if all(f(ip) for ip in net):
+        print(net.netmask)
+        #254
